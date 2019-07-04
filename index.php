3c69c1db47f812687e68f5161d09e2be396af49b
Juri Ehret <juri.ehret@gmail.com>
2019-07-04 06:55:27 +0200

    Another possibility to extend the Site-Class. The instance of the site class can be completely replaced with custom ones.

--- index.php
+++ index.php
@@ -7,10 +7,10 @@ include __DIR__.'/data/settings/scriptor-config.php';
 include __DIR__."/$config[admin_path]module.php";
 include __DIR__."/$config[admin_path]site.php";
 include __DIR__."/$config[admin_path]csrf.php";
-if(file_exists(__DIR__.'/site/theme/functions.php')) {
-	include __DIR__.'/site/theme/functions.php';
-}
-$page = new Site($config);
+
+$page = null;
+if(file_exists(__DIR__.'/site/theme/_ext.php')) { include __DIR__.'/site/theme/_ext.php';}
+else { $page = new Site($config); }
 $page->init();
 include __DIR__.'/site/theme/template.php';
 ?>
