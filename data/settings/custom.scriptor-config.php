7130f5c33cdce14f6763c830ba22f4bb28fc3e14
Juri Ehret <juri.ehret@gmail.com>
2020-02-11 09:00:22 +0100

    custom.scriptor-config.php template | guillotine icon

---
+++ data/settings/custom.scriptor-config.php
@@ -0,0 +1,8 @@
+<?php defined('IS_IM') or die('You cannot access this page directly');
+
+/*
+Enter here the configuration parameters that you want to overwrite, e.g:   
+$config = array_merge($config, [
+	'site_name' => 'My Websitename',
+]);
+*/
